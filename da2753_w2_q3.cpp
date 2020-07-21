4b107fec78573f2cc28191b09b680d1a5a4c83ea
Daniel <ardekanicapital@gmail.com>
2020-07-21 18:32:08 -0400

    creat total days, hours, and minutes, and output time to user.

--- da2753_w2_q3.cpp
+++ da2753_w2_q3.cpp
@@ -1,13 +1,49 @@
 #include <iostream>
 using namespace std;
 
+const int HOURS_IN_A_DAY = 24;
+const int MINUTES_IN_24HRS = 1440;
+const int MINUTES_IN_AN_HOUR = 60;
+
 int main()
 {
-    cout << "please enter the number of days John has worked: " < endl;
-    cout << "please enter the number of hours John has worked: " < endl;
-    cout << "please enter the number of minutes John has worked: " < endl;
+    // Total 
+    int totalDays, totalMinutes, totalHours, carryOverDays,
+    carryOverHours, carryOverMinutes, remainderMinutes;
+    // JOHN
+    int daysWorkedJohn, hoursWorkedJohn, minutesWorkedJohn;
+    // BILL
+    int daysWorkedBill, hoursWorkedBill, minutesWorkedBill;
+
+    cout << "please enter the number of days John has worked: ";
+    cin >> daysWorkedJohn;
+    cout << "please enter the number of hours John has worked: ";
+    cin >> hoursWorkedJohn;
+    cout << "please enter the number of minutes John has worked: ";
+    cin >> minutesWorkedJohn;
     cout << endl;
-    cout << "please enter the number of days Bill has worked: " < endl;
-    cout << "please enter the number of hours Bill has worked: " < endl;
-    cout << "please enter the number of minutes Bill has worked: " < endl;
+    cout << "please enter the number of days Bill has worked: ";
+    cin >> daysWorkedBill;
+    cout << "please enter the number of hours Bill has worked: ";
+    cin >> hoursWorkedBill;
+    cout << "please enter the number of minutes Bill has worked: ";
+    cin >> minutesWorkedBill;
+
+    // Minutes
+    totalMinutes = minutesWorkedBill + minutesWorkedJohn + remainderMinutes;
+    carryOverMinutes = totalMinutes / MINUTES_IN_AN_HOUR;
+    remainderMinutes = totalMinutes % MINUTES_IN_AN_HOUR;
+
+    // Hours
+    totalHours = hoursWorkedBill + hoursWorkedJohn + carryOverHours + carryOverMinutes;
+    carryOverDays = totalHours / HOURS_IN_A_DAY;
+    carryOverHours = totalHours % HOURS_IN_A_DAY;
+
+    // Days
+    totalDays = daysWorkedBill + daysWorkedJohn + carryOverDays;
+   
+   cout << "The total time both of them worked together is: " << totalDays << " days " << carryOverHours << " hours " << remainderMinutes << " minutes." << endl;
+
+   cout << carryOverMinutes << endl;
+   cout << remainderMinutes << endl;
 }
