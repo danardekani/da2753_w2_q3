afde8b124f617c51cbe0107c6ad038f3783af9c0
Daniel <ardekanicapital@gmail.com>
2020-07-21 17:18:42 -0400

    create I/O for user input data colleciton

---
+++ da2753_w2_q3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "please enter the number of days John has worked: " < endl;
+    cout << "please enter the number of hours John has worked: " < endl;
+    cout << "please enter the number of minutes John has worked: " < endl;
+    cout << endl;
+    cout << "please enter the number of days Bill has worked: " < endl;
+    cout << "please enter the number of hours Bill has worked: " < endl;
+    cout << "please enter the number of minutes Bill has worked: " < endl;
+}
